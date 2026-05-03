cf7c4209e2c51e15fd86d4cee870e019d0b18ec3
Kayla <kmrios00@icloud.com>
2026-05-03 14:24:05 -0500

    Import functions from various modules in __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,4 @@
+from .network_scan import scan_local_ports
+from .audit_check import check_windows_settings
+from .process_monitor import check_processes
+from .password_test import test_password_strength
