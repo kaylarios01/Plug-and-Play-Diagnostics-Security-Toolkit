7dca3295ca06a52617ef01fac0839a09d0035fa7
Kayla <kmrios00@icloud.com>
2026-05-05 11:53:36 -0500

    Clean up imports in __init__.py
    Remove unused import statements from __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,1 @@
-from .network_scan import scan_local_ports
-from .audit_check import check_windows_settings
-from .process_monitor import check_processes
-from .password_test import test_password_strength
+
